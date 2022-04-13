f1fe16fe4063fd6983a5c97194cadaf75ed93dc1
imskanand <the.anand3099@gmail.com>
2022-04-13 16:58:12 +0530

    Initial Develoopmet of the Flutter App - Added the main functionlity - Added the MaterialApp() functionality

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// The main function is the starting point of the flutter app
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hi Maa'),
+      ),
+    ),
+  );
+}
