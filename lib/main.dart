f97a026e1b9c8547e1fe92d1bbd1b1b317ccb78c
imskanand <the.anand3099@gmail.com>
2022-04-13 18:31:36 +0530

    Flutter App Completed ++ Added the Image View ++ Added the app icon ++ Added the Image to be find in the asset folder so that we can execute it esily ++ Learned basic of flutter app development

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hi Maa'),
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('I Am Rich'),
+        backgroundColor: Colors.blueGrey[900],
       ),
-    ),
+      backgroundColor: Colors.blueGrey,
+      body: const Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
+        ),
+      ),
+    )),
   );
 }
